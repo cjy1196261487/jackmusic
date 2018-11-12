668e33ca7a3234985fcfb00878eeca7b1646bf3d
cjy1196261487 <il>
2018-11-12 18:45:50 +0800

    third

--- app/src/main/java/com/cjy/jackmusic/activity/MusicActivity.java
+++ app/src/main/java/com/cjy/jackmusic/activity/MusicActivity.java
@@ -1,17 +1,24 @@
 package com.cjy.jackmusic.activity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.cjy.jackmusic.R;
+import com.cjy.jackmusic.adapter.MusicAdapter;
 import com.cjy.jackmusic.utils.OkHttpUtil;
+import com.cjy.jackmusic.utils.SearchUtil;
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.google.gson.Gson;
 
 import java.io.IOException;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -20,6 +27,10 @@ import okhttp3.Response;
 public class MusicActivity extends AppCompatActivity {
     private ImageView backbutton,serachbutton;
     private EditText songname;
+    private  SearchUtil searchUtil ;
+    private ListView searchlist;
+    private MediaPlayer mediaPlayer=new MediaPlayer();
+     private List<SearchUtil.ResultBean>searchUtilList;
 
 
 
@@ -30,7 +41,7 @@ public class MusicActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fresco.initialize(this);
+
         setContentView(R.layout.musiclist_layout);
         initView();
 
@@ -39,15 +50,21 @@ public class MusicActivity extends AppCompatActivity {
 
     }
 
+    private void initView() {
+        backbutton = findViewById(R.id.backbutton);
+        songname = findViewById(R.id.music_search);
+        serachbutton = findViewById(R.id.button_search);
+        searchlist = findViewById(R.id.music_list);
+
+    }
+
     private void initListen() {
         serachbutton.setOnClickListener(new View.OnClickListener() {
-
-
             @Override
             public void onClick(View view) {
-                String songorsing=songname.getText().toString();
-                String requestUrl="http://api.apiopen.top/searchMusic?name="+songorsing;
-                OkHttpUtil.sendOkhttpResquest(requestUrl, new Callback() {
+                String name=songname.getText().toString();
+                String url="http://api.apiopen.top/searchMusic?name="+name;
+                OkHttpUtil.sendOkhttpResquest(url, new Callback() {
                     @Override
                     public void onFailure(Call call, IOException e) {
 
@@ -55,24 +72,46 @@ public class MusicActivity extends AppCompatActivity {
 
                     @Override
                     public void onResponse(Call call, Response response) throws IOException {
-                        Log.e("歌曲详情",response.body().string());
+                        String strrespone=response.body().string();
+                        searchUtil=new Gson().fromJson(strrespone,SearchUtil.class);
+                        searchUtilList=searchUtil.getResult();
+                        if (searchUtil.getCode()==200){
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    MusicAdapter musicAdapter=new MusicAdapter(MusicActivity.this,searchUtilList);
+                                    searchlist.setAdapter(musicAdapter);
+                                    musicAdapter.notifyDataSetChanged();
+                                }
+                            });
+                        }
+
 
                     }
                 });
-
             }
         });
+        searchlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+
+                mediaPlayer.reset();
+
+                try {
+                    mediaPlayer.setDataSource(searchUtilList.get(i).getUrl());
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+
+
 
 
     }
-
-    private void initView() {
-        backbutton=findViewById(R.id.backbutton);
-        songname=findViewById(R.id.music_search);
-        serachbutton= findViewById(R.id.button_search);
-
-
-    }
-
-
-}
---
+++ app/src/main/java/com/cjy/jackmusic/adapter/MusicAdapter.java
@@ -0,0 +1,61 @@
+package com.cjy.jackmusic.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.cjy.jackmusic.R;
+import com.cjy.jackmusic.activity.MusicActivity;
+import com.cjy.jackmusic.utils.SearchUtil;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import java.util.IdentityHashMap;
+import java.util.List;
+
+public class MusicAdapter extends BaseAdapter{
+    private Context context;
+    private List<SearchUtil.ResultBean>resultBeans;
+
+    public MusicAdapter(Context context, List<SearchUtil.ResultBean> searchUtilList) {
+        this.context=context;
+        resultBeans=searchUtilList;
+    }
+
+    @Override
+    public int getCount() {
+        return resultBeans.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return resultBeans.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        view= LayoutInflater.from(context).inflate(R.layout.musiclist_item,viewGroup,false);
+        SimpleDraweeView simpleDraweeView=view.findViewById(R.id.musiclist_pic);
+        TextView songname=view.findViewById(R.id.songname);
+        TextView singname=view.findViewById(R.id.sing_name);
+        simpleDraweeView.setImageURI(resultBeans.get(i).getPic());
+        songname.setText(resultBeans.get(i).getTitle());
+
+        if (resultBeans.get(i).getAuthor().length()>20){
+            singname.setText(resultBeans.get(i).getAuthor().substring(0,9)+".....");
+        }else {
+            singname.setText(resultBeans.get(i).getAuthor());
+        }
+
+
+
+        return view;
+    }
+}
